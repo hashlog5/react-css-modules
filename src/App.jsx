ba5a2672b4a37e85c826846654ac139d6229be08
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-04 16:40:05 -0700

    Luxury.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Find from './components/find/Find';
 import Driver from './components/driver/Driver';
+import Luxury from './components/luxury/Luxury';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Find />
       <Driver />
+      <Luxury />
     </div>
   );
 };
